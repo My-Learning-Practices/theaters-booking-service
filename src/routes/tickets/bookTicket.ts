9e5530049926653f4093bca629e9cb8323cc0da6
Sumit Singh <sumit.singh@tango340b.com>
2022-10-15 13:43:57 +0530

    Refactor: Impored code readability

--- src/routes/tickets/bookTicket.ts
+++ src/routes/tickets/bookTicket.ts
@@ -3,14 +3,14 @@ import { RequestHandler, Request, Response } from "express";
 const ticket = require("../../database/schemas/ticket");
 
 const bookTicket: RequestHandler = async (req: Request, res: Response) => {
-  const ticketDetails = req.body?.ticketDetails;
+    const ticketDetails = req.body?.ticketDetails;
 
-  try {
-    const newTicket = await ticket.create(ticketDetails);
-    console.log(newTicket);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const newTicket = await ticket.create(ticketDetails);
+        console.log(newTicket);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export default bookTicket;
